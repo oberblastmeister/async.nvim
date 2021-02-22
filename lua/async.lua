5659fb79c3a1d53ed03468a44f0358a779d10923
Brian Shu <littlebubu.shu@gmail.com>
2021-02-22 14:12:57 -0500

    added multival return

---
+++ lua/async.lua
@@ -0,0 +1,94 @@
+local co = coroutine
+
+-- use with wrap
+local pong = function (func, callback)
+  assert(type(func) == "function", "type error :: expected func")
+  local thread = co.create(func)
+  local step = nil
+  step = function (...)
+    local res = {co.resume(thread, ...)}
+    local stat = res[1]
+    local ret = {select(2, unpack(res))}
+    assert(stat, ret)
+    if co.status(thread) == "dead" then
+      (callback or function () end)(unpack(ret))
+    else
+      assert(#ret == 1, "expected a single return value")
+      assert(type(ret[1]) == "function", "type error :: expected func")
+      ret[1](step)
+    end
+  end
+  step()
+end
+
+
+-- use with pong, creates thunk factory
+local wrap = function (func)
+  assert(type(func) == "function", "type error :: expected func")
+  local factory = function (...)
+    local params = {...}
+    local thunk = function (step)
+      table.insert(params, step)
+      return func(unpack(params))
+    end
+    return thunk
+  end
+  return factory
+end
+
+
+-- many thunks -> single thunk
+local join = function (thunks)
+  local len = #thunks
+  local done = 0
+  local acc = {}
+
+  local thunk = function (step)
+    if len == 0 then
+      return step()
+    end
+    for i, tk in ipairs(thunks) do
+      assert(type(tk) == "function", "thunk must be function")
+      local callback = function (...)
+        acc[i] = {...}
+        done = done + 1
+        if done == len then
+          step(unpack(acc))
+        end
+      end
+      tk(callback)
+    end
+  end
+  return thunk
+end
+
+
+-- sugar over coroutine
+local await = function (defer)
+  assert(type(defer) == "function", "type error :: expected func")
+  return co.yield(defer)
+end
+
+
+local await_all = function (defer)
+  assert(type(defer) == "table", "type error :: expected table")
+  return co.yield(join(defer))
+end
+
+local async = wrap(pong)
+
+local async_wrap = function(func)
+  return function(...)
+    local args = {...}
+    return async(function()
+      return func(unpack(args))
+    end)
+  end
+end
+
+return {
+  sync = async_wrap,
+  wait = await,
+  wait_all = await_all,
+  wrap = wrap,
+}
